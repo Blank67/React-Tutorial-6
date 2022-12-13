5c574fb9f5995ce40fc7072ca9896d12370f2513
Blank67 <hmnshshrm67@gmail.com>
2022-12-13 16:42:15 +0530

    converted into async await

--- src/App.js
+++ src/App.js
@@ -7,35 +7,30 @@ const App = () => {
 
   const [movies, setMovies] = useState([]);
 
-    const fetchMovieHandler = () => {
-        fetch("https://swapi.dev/api/films")
-            .then((res) => {
-              return res.json(); //Convert data from Json into obj
-            })
-            .then((data) => {
-              const updatedMovies = data.results.map((itm) =>{
-                return {
-                  id: itm.episode_id,
-                  title: itm.title,
-                  openingText: itm.opening_crawl,
-                  releaseDate: itm.release_date
-                }
-              });
-              setMovies(updatedMovies);
-            })
-            .catch((err) => console.log("Fetch Error", err));
-    };
+  const fetchMovieHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const updatedMovies = data.results.map((itm) => {
+      return {
+        id: itm.episode_id,
+        title: itm.title,
+        openingText: itm.opening_crawl,
+        releaseDate: itm.release_date
+      }
+    });
+    setMovies(updatedMovies);
+  };
 
-    return (
-        <React.Fragment>
-            <section>
-                <button onClick={fetchMovieHandler}>Fetch Movies</button>
-            </section>
-            <section>
-                <MoviesList movies={movies} />
-            </section>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
 };
 
 export default App;
